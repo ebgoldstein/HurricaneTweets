275aa9ff4f4d8073fde0346676d7e3523bc6c33c
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2019-03-21 12:10:55 -0400

    Update IrmaShiny.R

--- IrmaShiny.R
+++ IrmaShiny.R
@@ -3,10 +3,7 @@
 
 #https://rstudio.github.io/leaflet/shiny.html
 
-#install package to map (leaflet),  save html (htmlwidgets).drawing objects (sp)
-#install.packages(c("shiny", "leaflet", "htmlwidgets", "sp", "tidyverse"))
-
-##get leaflet
+#Install the needed libraries
 library(shiny)
 library(leaflet)
 library(htmlwidgets)
@@ -16,12 +13,18 @@ library(mapview)
 library(shiny)
 library(RColorBrewer)
 
-#import the IRMA tweets
+#import the IRMA tweets (n.b., after the hydration step)
 Irma_Tweets <- read_csv("~/Irma_Tweets.csv")
 
-#URL field
+#URL field for each tweet
 URL  = paste0("'","<a"," href", "=", Irma_Tweets$image_url , " target = '_blank'", ">", "TWEET</a>","'") #create hyperlinks from tweets
 
+########################################
+#build the Shiny UI with 4 sliders, color input, and legend check box: 
+# S1) Image Score
+# S2) Text Score
+# S3) User Score
+# S4) GIS Score
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
@@ -39,10 +42,11 @@ ui <- bootstrapPage(
                 checkboxInput("legend", "Show legend", TRUE)
   )
 )
+#########################
 
 server <- function(input, output, session) {
   
-  # Reactive expression for the data subsetted to what the user selected
+  # These are the 4 reactive expressions for the sliders
   filteredData <- reactive({
     Irma_Tweets[Irma_Tweets$imageScore >= input$range[1] & Irma_Tweets$imageScore <= input$range[2] &
                   Irma_Tweets$textScore >= input$rangetwo[1] & Irma_Tweets$textScore <= input$rangetwo[2] & 
@@ -51,47 +55,33 @@ server <- function(input, output, session) {
                 ,]
   })
   
-  # This reactive expression represents the palette function,
-  # which changes as the user makes selections in UI.
+  # Reactive expression for the slider color, with colors coresponding to image score. 
   colorpal <- reactive({
     colorNumeric(input$colors, Irma_Tweets$imageScore)
   })
   
   output$map <- renderLeaflet({
-    # Use leaflet() here, and only include aspects of the map that
-    # won't need to change dynamically (at least, not unless the
-    # entire map is being torn down and recreated).
+    # Make the leaflet map. Pull the basemap from OSM. 
     leaflet(Irma_Tweets) %>% 
       addProviderTiles(providers$OpenStreetMap) %>%
       fitBounds(~min(longitude), ~min(latitude), ~max(longitude), ~max(latitude))
   })
   
-  # Incremental changes to the map (in this case, replacing the
-  # circles when a new color is chosen) should be performed in
-  # an observer. Each independent set of things that can change
-  # should be managed in its own observer.
+  # observer for sliders:
   observe({
     pal <- colorpal()
-    
     leafletProxy("map", data = filteredData()) %>%
       clearShapes() %>%
       addCircles(
-                       #lng = Irma_Tweets$longitude, 
-                       #lat = Irma_Tweets$latitude,
-                       
-                       #popup=popupImage(Irma_Tweets$image_url,embed=TRUE,height=200),
-                       #popup = paste0(Irma_Tweets$text, "<img height='200' src = ", Irma_Tweets$image_url, ">"),
-                       
-                       stroke = TRUE, radius = 2000, weight = 1, color = "#777777",
-                       fillColor = ~pal(imageScore), fillOpacity = 0.7,
-                       popup = ~paste(text, "<img height='200' src = ", image_url, ">")
+        stroke = TRUE, radius = 2000, weight = 1, color = "#777777",
+        fillColor = ~pal(imageScore), fillOpacity = 0.7,
+        popup = ~paste(text, "<img height='200' src = ", image_url, ">")
       )
   })
   
-  # Use a separate observer to recreate the legend as needed.
+  #Observer for legend:
   observe({
     proxy <- leafletProxy("map", data = Irma_Tweets)
-    
     # Remove any existing legend, and only if the legend is
     # enabled, create a new one.
     proxy %>% clearControls()
@@ -104,6 +94,7 @@ server <- function(input, output, session) {
   })
 }
 
+#Make it real:
 shinyApp(ui, server)
 
 
