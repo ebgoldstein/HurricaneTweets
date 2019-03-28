355c1565c572ab60ed3c62bd727c6dc33454023d
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2019-03-28 12:52:38 -0400

    Update IrmaShiny.R

--- IrmaShiny.R
+++ IrmaShiny.R
@@ -20,6 +20,9 @@ Irma_Tweets <- read_csv("~/Irma_Tweets.csv")
 #jitter coordinates for IRMA tweets to avoid overlap (can click on overlapping tweets if zoomed in enough)
 Irma_Tweets[3:4] <- jitter(Irma_Tweets[3:4], factor = 0.001)
 
+#scale text scores from 0 to 1
+Irma_Tweets$textScore <- (Irma_Tweets$textScore- min(Irma_Tweets$textScore))/(max(Irma_Tweets$textScore)-min(Irma_Tweets$textScore))
+
 #order by image score so higher scores are on top
 Irma_Tweets <- Irma_Tweets[order(Irma_Tweets$imageScore),]
 
@@ -37,6 +40,9 @@ IrmaTrack$log <- as.numeric(IrmaTrack$log)
 #URL field for each tweet
 URL  = paste0("'","<a"," href", "=", Irma_Tweets$image_url , " target = '_blank'", ">", "TWEET</a>","'") #create hyperlinks from tweets
 
+
+pal <-   colorNumeric("viridis", c(0,1))
+
 ########################################
 #build the Shiny UI with 4 sliders, color input, and legend check box: 
 # S1) Image Score
@@ -46,17 +52,15 @@ URL  = paste0("'","<a"," href", "=", Irma_Tweets$image_url , " target = '_blank'
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%} img {display:block;margin-left:auto;margin-right:auto;width:80%;}"),
   leafletOutput("map", width = "100%", height = "100%"),
-  absolutePanel(top = 10, right = 10,
+  absolutePanel(top = 120, left = 10,
                 sliderInput("range", "Image Score", 0,1,
                             value = range(Irma_Tweets$imageScore), step = 0.1),
-                sliderInput("rangetwo", "Text Score", 0,100,
+                sliderInput("rangetwo", "Text Score", 0,1,
                             value = range(Irma_Tweets$textScore), step = 0.1),
                 sliderInput("rangethree", "User Score", 0,1,
                             value = range(Irma_Tweets$userScore), step = 0.1),
                 sliderInput("rangefour", "GIS Score", 0,1,
                             value = range(Irma_Tweets$gisScore), step = 0.1),
-                #selectInput("colors", "Color Scheme",
-                #rownames(subset(brewer.pal.info, category %in% c("seq", "div")))),
                 checkboxInput("legend", "Show legend", TRUE)
   )
 )
@@ -73,10 +77,6 @@ server <- function(input, output, session) {
                 ,]
   })
   
-  # Reactive expression for the slider color, with colors corresponding to image score. 
-  colorpal <- reactive({
-    colorNumeric("viridis", Irma_Tweets$imageScore)
-  })
   
   output$map <- renderLeaflet({
     # Make the leaflet map. Pull the basemap. Changed to Stamen TonerLite for simplicity, but I wish it had color :( -JK 
@@ -88,7 +88,6 @@ server <- function(input, output, session) {
   
   # observer for sliders:
   observe({
-    pal <- colorpal()
     leafletProxy("map", data = filteredData()) %>%
       clearMarkers() %>%
       addCircleMarkers(
@@ -114,7 +113,7 @@ server <- function(input, output, session) {
     # enabled, create a new one.
     proxy %>% clearControls()
     if (input$legend) {
-      pal <- colorpal()
+      #pal <- colorpal()
       proxy %>% addLegend(position = "bottomright",
                           pal = pal, values = ~imageScore
       )
@@ -124,3 +123,4 @@ server <- function(input, output, session) {
 
 #Make it real:
 shinyApp(ui, server)
+
